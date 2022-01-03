91eaee0cd411f8446f94924677d9fa93327f3a94
maciejka1441 <84379513+maciejka1441@users.noreply.github.com>
2022-01-03 18:09:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 3000;
 var id = { name: "Maciej", surname: "Piekarski", class: "3I2", group: "a" }
 
 app.get("/", function (req, res) {
-    res.send("<h2 style='color: blue; font-family: arial; text-align: center; margin-top: 20px;'>My app!</h2>")
+    res.send("<h1 style='color: blue; font-family: arial; text-align: center; margin-top: 20px;'>My app!</h1>")
 })
 
 app.get("/data", function (req, res) {
