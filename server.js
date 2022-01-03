d809da5f7c4c05e935cd0f67bac3eadddfda5bd2
maciejka1441 <maciej1441@gmail.com>
2022-01-03 17:40:35 +0100

    Server extension for send personal data

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var express = require("express")
 var app = express()
 var PORT = process.env.PORT || 3000;
 
+var id = { name: "Maciej", surname: "Piekarski", class: "3I2", group: "a" }
+
 app.get("/", function (req, res) {
-    res.send("<h1>My app on heroku!</h1>")
+    res.send("<h2 style='color: blue; font-family: arial; text-align: center; margin-top: 20px;'>My app!</h2>")
+})
+
+app.get("/data", function (req, res) {
+    res.send(id)
 })
 
 app.listen(PORT, function () {
